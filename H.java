c6920d4a22e24f4bd0a188c8541ed2b5a3c5cdd5
viplaw02 <viplawsinha1998@gmail.com>
2024-12-25 13:39:18 +0530

    commit

---
+++ H.java
@@ -0,0 +1,10 @@
+public class H {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=5;j>i;j--){
+                System.out.print("j");
+            }
+            System.out.println();
+    }
+}
+}
