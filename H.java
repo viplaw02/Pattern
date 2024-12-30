f5e604ad181d6ffea429322fce8f8a3d14ffcfbb
Viplaw Sinha <148614032+viplaw02@users.noreply.github.com>
2024-12-30 15:29:09 +0530

    Update H.java

--- H.java
+++ H.java
@@ -2,7 +2,7 @@ public class H {
     public static void main(String[] args) {
         for(int i=0;i<5;i++){
             for(int j=5;j>i;j--){
-                System.out.print("j");
+                System.out.print("*");
             }
             System.out.println();
     }
