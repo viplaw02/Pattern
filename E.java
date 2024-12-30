bf2d0aec78e63247b4a67e118be72aa4d0cea9e1
Viplaw Sinha <148614032+viplaw02@users.noreply.github.com>
2024-12-30 15:27:12 +0530

    Update E.java

--- E.java
+++ E.java
@@ -2,7 +2,7 @@ public class E {
     public static void main(String[] args) {
         for(int i =0;i<6;i++){
           for( int j=6;j>i;j--){
-            System.out.print("k");
+            System.out.print("*");
           }
           System.out.println();
         }
